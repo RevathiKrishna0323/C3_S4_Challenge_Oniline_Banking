92f43ba57eb691e97334f43fcb3ef59c7c13b302
Revathi G <revathikrishna0323@gmail.com>
2022-08-29 21:46:13 +0530

    created no args constructor

---
+++ src/main/java/com/niit/jdp/SavingsAccount.java
@@ -0,0 +1,15 @@
+/*
+ * Author Name: Revathi
+ * Date: 29-08-2022
+ * Created With: IntelliJ IDEA Community Edition
+ */
+
+package com.niit.jdp;
+
+public class SavingsAccount extends Bank {
+    //created a no args constructor
+
+    public SavingsAccount() {
+    }
+
+}
