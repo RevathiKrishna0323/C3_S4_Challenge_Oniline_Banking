563f04420e08f09b2296571abec325bb77a1ba66
Revathi G <revathikrishna0323@gmail.com>
2022-08-29 21:48:01 +0530

    created parameterised constructor

--- src/main/java/com/niit/jdp/SavingsAccount.java
+++ src/main/java/com/niit/jdp/SavingsAccount.java
@@ -11,5 +11,10 @@ public class SavingsAccount extends Bank {
 
     public SavingsAccount() {
     }
+    // created a parameterised constructor
+
+    public SavingsAccount(String accountNumber, String accountType, String accountName, String bankName, String branchName, String address, String IFSC_Code, String city, String accountOpenDate, String modeOfOperation, String bankCode, String bankCountry, String bankState, String bankCountryCode, String bankCountryName, String bankStateCode, String bankStateName) {
+        super(accountNumber, accountType, accountName, bankName, branchName, address, IFSC_Code, city, accountOpenDate, modeOfOperation, bankCode, bankCountry, bankState, bankCountryCode, bankCountryName, bankStateCode, bankStateName);
+    }
 
 }
