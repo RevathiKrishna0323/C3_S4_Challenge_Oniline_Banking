00d485e38cabfc568d3df13c3eb9858eb1600cc6
Revathi G <revathikrishna0323@gmail.com>
2022-08-29 21:35:57 +0530

    defined the fields of the Bank object

--- src/main/java/com/niit/jdp/Bank.java
+++ src/main/java/com/niit/jdp/Bank.java
@@ -6,5 +6,23 @@
 
 package com.niit.jdp;
 
-public class Bank {
+public abstract class Bank {
+    // define the fields of the Bank object
+    private String accountNumber;
+    private String accountType;
+    private String accountName;
+    private String bankName;
+    private String branchName;
+    private String address;
+    private String IFSC_Code;
+    private String city;
+    private String accountOpenDate;
+    private String modeOfOperation;
+    private String bankCode;
+    private String bankCountry;
+    private String bankState;
+    private String bankCountryCode;
+    private String bankCountryName;
+    private String bankStateCode;
+    private String bankStateName;
 }
